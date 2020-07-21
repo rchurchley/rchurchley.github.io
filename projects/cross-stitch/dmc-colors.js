3328b24690977e7a5ada8dee6289148c02a77637
Ross Churchley <ross@rosschurchley.com>
2020-07-20 18:42:13 -0700

    fix(crossstitch): wrong JASC-PAL length

--- projects/cross-stitch/dmc-colors.js
+++ projects/cross-stitch/dmc-colors.js
@@ -3,17 +3,17 @@ let swatch = document.querySelector('#swatch');
 let pal = document.querySelector('#pal');
 
 d3.csv("/projects/cross-stitch/DMC.csv")
-  .then( (data) => {
+  .then((data) => {
     // data from https://krestiki.at.ua/color_table_d.html, which appears to be consistent with Stitchly
-    data = data.filter( (r) => (r.k && (r.k != '') ) )
-      .map( (r) => ( { code: r.code, name: r.name, have: (r.have == 'true'), color: chroma(r.k) } ))
-      .sort( (x,y) => (
-        Math.round((y.color.hsl()[0] - x.color.hsl()[0])/30) +
-        (x.color.lch()[0] - y.color.lch()[0]) / 100 + 
+    data = data.filter((r) => (r.k && (r.k != '')))
+      .map((r) => ({ code: r.code, name: r.name, have: (r.have == 'true'), color: chroma(r.k) }))
+      .sort((x, y) => (
+        Math.round((y.color.hsl()[0] - x.color.hsl()[0]) / 30) +
+        (x.color.lch()[0] - y.color.lch()[0]) / 100 +
         Math.round((1 / x.color.lch()[1] - 1 / y.color.lch()[1]) * 2) * 10
       ))
 
-    data.forEach(r => {      
+    data.forEach(r => {
       let row = swatch.content.cloneNode(true);
       let tag = row.querySelector(".swatch");
       tag.classList.add(`dmc-${r.code}`)
@@ -23,6 +23,6 @@ d3.csv("/projects/cross-stitch/DMC.csv")
       root.appendChild(row)
     });
 
-    palFile = ["JASC-PAL", "0100", `${data.length}`].concat(data.filter(r => r.have).map( (r) => r.color.rgb().join(" ") ));
+    palFile = ["JASC-PAL", "0100", `${data.filter(r => r.have).length}`].concat(data.filter(r => r.have).map((r) => r.color.rgb().join(" ")));
     pal.textContent = palFile.join("\n")
   });
